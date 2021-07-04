d2c3665a8812a0566e53d61262435bcb10d5d43a
MuratHaktanir <79758626+MuratHaktanir@users.noreply.github.com>
2021-07-04 18:17:10 +0300

    Initial Commit

---
+++ Light/LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightApp.swift
+//  Light
+//
+//  Created by Murat Haktanır on 4.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
