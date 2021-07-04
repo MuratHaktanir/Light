cc58026feacd882954d905dfb2706b581070b353
MuratHaktanir <79758626+MuratHaktanir@users.noreply.github.com>
2021-07-04 21:07:43 +0300

    Update ContentView.swift
    Learning swiftUI!

--- Light/ContentView.swift
+++ Light/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isNight = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            BackgroundView(isNight: $isNight)
+            VStack {
+                Text("Your light for midnight!")
+                    .font(.title)
+                    .foregroundColor(isNight ? .black : .white)
+            }
+            
+            // Accessibility
+            .accessibilityElement(children: .ignore)
+            .accessibility(label: Text("Your light for midnight!"))
+            
+        }.onTapGesture {
+            isNight.toggle()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        Color(isNight ? .white : .black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
